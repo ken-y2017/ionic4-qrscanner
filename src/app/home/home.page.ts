4de9aee28ef338ac7ec3e5424bd1cb67f1b87b50
Yoshida Kenichiro <ken.y201701@gmail.com>
2019-10-16 06:35:51 +0900

    Implement QR Scanner

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { QRScanner, QRScannerStatus } from '@ionic-native/qr-scanner/ngx';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,43 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  isOpen = false;
+  scannedData: {};
 
-  constructor() {}
+  constructor(
+    private qrScanner: QRScanner,
+  ) {
+  }
+
+  openScanner() {
+    this.qrScanner.prepare()
+      .then((status: QRScannerStatus) => {
+        console.log(status);
+        if (status.authorized) {
+          this.isOpen = true;
+
+          // start scanning
+          const scanSub = this.qrScanner.scan().subscribe((text: string) => {
+            console.log('Scanned something', text);
+
+            this.isOpen = false;
+            this.qrScanner.hide().then();
+            scanSub.unsubscribe();
+          });
+
+          this.qrScanner.show().then();
+
+
+        } else if (status.denied) {
+          // camera permission was permanently denied
+          // you must use QRScanner.openSettings() method to guide the user to the settings page
+          // then they can grant the permission from there
+          this.qrScanner.openSettings();
+        } else {
+          // permission was denied, but not permanently. You can ask for permission again at a later time.
+        }
+      })
+      .catch((e: any) => console.error(e));
+  }
 
 }
